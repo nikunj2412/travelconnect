70743b18d06f59b79fcc7c2f21606e7a8001cf6c
sanketnaliyadra <sanketnaliyadra00@gmail.com>
2024-04-18 18:30:41 -0400

    => dashboard chnages

--- Travel-admin/src/components/Dashboard/DashboardStats.jsx
+++ Travel-admin/src/components/Dashboard/DashboardStats.jsx
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoBagHandle, IoPieChart, IoPeople, IoCart } from 'react-icons/io5'
 
 export default function DashboardStats() {
+	const [totalBookingValue, setTotalBookingValue] = useState(0);
+    const [totalPackages, setTotalPackages] = useState(0);
+    const [totalBooking, setTotalBooking] = useState(0);
+	const [totalBookingNum, setTotalBookingNum] = useState(0);
+
+    useEffect(() => {
+        fetchBookingData();
+		fetchTravelPosts();
+		fetchBooking();
+		fetchTotalBooking();
+    }, []);
+
+    const fetchBookingData = async () => {
+        try {
+            const response = await fetch('https://travelconnect.onrender.com/v1/admin/getAllBookings');
+            if (!response.ok) {
+                throw new Error('Failed to fetch booking data');
+            }
+            const {data} = await response.json();
+            console.log("DASH",data)
+            const bookingValue = calculateTotalBookingValue(data);
+            setTotalBookingValue(bookingValue);
+        } catch (error) {
+            console.error('Error fetching booking data:', error);
+        }
+    };
+
+	const fetchTravelPosts = async () => {
+        try {
+            const response = await fetch('https://travelconnect.onrender.com/v1/admin/travel-posts');
+            if (!response.ok) {
+                throw new Error('Failed to fetch travel posts');
+            }
+            const data = await response.json();
+            // Calculate total number of packages
+            const numPackages = data.data.length;
+            setTotalPackages(numPackages);
+        } catch (error) {
+            console.error('Error fetching travel posts:', error);
+        }
+    };
+
+	const fetchBooking = async () => {
+        try {
+            const response = await fetch('https://travelconnect.onrender.com/v1/admin/');
+            if (!response.ok) {
+                throw new Error('Failed to fetch travel posts');
+            }
+            const data = await response.json();
+            const numbooking = data.data.length;
+            setTotalBooking(numbooking);
+        } catch (error) {
+            console.error('Error fetching travel posts:', error);
+        }
+    };
+
+	const fetchTotalBooking = async () => {
+        try {
+            const response = await fetch('https://travelconnect.onrender.com/v1/admin/getAllBookings');
+            if (!response.ok) {
+                throw new Error('Failed to fetch travel posts');
+            }
+            const data = await response.json();
+            const numbooking = data.data.length;
+            setTotalBookingNum(numbooking);
+        } catch (error) {
+            console.error('Error fetching travel posts:', error);
+        }
+    };
+    const calculateTotalBookingValue = (bookings) => {
+        return bookings.reduce((total, booking) => {
+            return total + booking.totalPrice;
+        }, 0);
+    };
 	return (
 		<div className="flex gap-4">
 			<BoxWrapper>
@@ -11,7 +85,7 @@ export default function DashboardStats() {
 				<div className="pl-4">
 					<span className="text-sm text-gray-500 font-light">Booking Value</span>
 					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">$5423</strong>
+						<strong className="text-xl text-gray-700 font-semibold">&#x24;{totalBookingValue}</strong>
 					</div>
 				</div>
 			</BoxWrapper>
@@ -22,7 +96,7 @@ export default function DashboardStats() {
 				<div className="pl-4">
 					<span className="text-sm text-gray-500 font-light">Total Packages</span>
 					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">7</strong>
+						<strong className="text-xl text-gray-700 font-semibold">{totalPackages}</strong>
 					</div>
 				</div>
 			</BoxWrapper>
@@ -33,7 +107,7 @@ export default function DashboardStats() {
 				<div className="pl-4">
 					<span className="text-sm text-gray-500 font-light">Total Customers</span>
 					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">34</strong>
+						<strong className="text-xl text-gray-700 font-semibold">{totalBooking}</strong>
 					</div>
 				</div>
 			</BoxWrapper>
@@ -44,7 +118,7 @@ export default function DashboardStats() {
 				<div className="pl-4">
 					<span className="text-sm text-gray-500 font-light">Total Booking</span>
 					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">8</strong>
+						<strong className="text-xl text-gray-700 font-semibold">{totalBookingNum}</strong>
 					</div>
 				</div>
 			</BoxWrapper>
